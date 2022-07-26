c8564516cb33c13ee005c13f3bbbaf22236718aa
ilysim <illymarie1@live.com>
2022-07-26 08:28:20 -0500

    fix notes

--- models/note.js
+++ models/note.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose'
 
+const Schema = mongoose.Schema
+
 const noteSchema = new mongoose.Schema({
   unit: {type: Number, min: 1, max: 4, default: 1, required: true},
   content: String,
