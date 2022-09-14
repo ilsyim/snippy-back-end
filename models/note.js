72a6438bdde305be9cd8a3c67bfa18602dc4d9fa
ilysim <illymarie1@live.com>
2022-09-14 13:21:56 -0500

    remove required

--- models/note.js
+++ models/note.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const noteSchema = new mongoose.Schema({
-  unit: {type: Number, min: 1, max: 4, default: 1, required: true},
+  unit: {type: Number, min: 1, max: 4, default: 1},
   content: String,
   owner: {type: Schema.Types.ObjectId, ref: 'Profile'},
   videoId: "String"
