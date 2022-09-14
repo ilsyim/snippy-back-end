6b750b0e9f5658fa65caa37e37f81ca3e2406af7
Kathy White <kawhite8503@gmail.com>
2022-09-14 11:56:52 -0500

    change model

--- models/note.js
+++ models/note.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema
 const noteSchema = new mongoose.Schema({
   unit: {type: Number, min: 1, max: 4, default: 1, required: true},
   content: String,
-  owner: {type: Schema.Types.ObjectId, ref: 'Profile'}
+  owner: {type: Schema.Types.ObjectId, ref: 'Profile'},
+  videoId: "String"
 },{
   timestamps: true,
 })
